317e1151ef50da0ffd04d175af587b5c7e76a819
Pedro <pedrofigueiraboaviagem@gmail.com>
2024-03-24 14:14:21 -0300

    async function beginning

--- src/components/Main/Boxes/index.tsx
+++ src/components/Main/Boxes/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
+const correcoes = [
+    {
+        palavra: 'original',
+        correcao: 'corrigido',
+        explicacao: 'explicacao'
+    }
+]
+
 const InputBox = () => {
     return (
         <div className="w-1/2 border-black">
@@ -16,6 +24,13 @@ const OutPutBox = () => {
 };
 
 const Boxes = () => {
+
+    async function apiCall() {
+        const response = await fetch('http://localhost:3000/correcoes');
+        const data = await response.json();
+        console.log(data);
+    }
+
     return (
         <div className="w-1/2 border-5">
             <InputBox />
--- src/components/Main/Buttons/index.tsx
+++ src/components/Main/Buttons/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Buttons = () => {
+
     return (
         <div>
             <button>Botão 1</button>
