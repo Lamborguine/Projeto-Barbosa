25d8dcb45175f4dde8a55742033740669a460127
Pedro <pedrofigueiraboaviagem@gmail.com>
2024-03-24 21:03:11 -0300

    Nome dos botões mudados

--- src/components/Main/Buttons/index.tsx
+++ src/components/Main/Buttons/index.tsx
@@ -8,20 +8,20 @@ const Buttons = () => {
                 <button
                     className="flex border-2 border-black hover:bg-gray-200 font-bold py-4 px-4 rounded-lg">
                     <img src={"/baixar.png"} alt="Baixar" className="h-6 w-6 inline-block mr-3 transform rotate-180" />
-                    <p>Upload de Arquivo</p>
+                    <p>Upload de Arquivo de Texto</p>
                 </button>
             </div>
             <div>
                 <button
                     className="bg-green-700 hover:bg-green-800 text-white font-bold py-4 px-7 rounded-lg" >
-                    Formatar Texto
+                    Corrigir Texto
                 </button>
             </div>
             <div className="justify-self-center">
                 <button
                     className="flex border-2 border-black hover:bg-gray-200 font-bold py-4 px-4 rounded-lg">
                     <img src={"/baixar.png"} alt="Baixar" className="h-6 w-6 inline-block mr-3" />
-                    <p>Download de Arquivo</p>
+                    <p>Download do Texto Corrigido</p>
                 </button>
             </div>
         </div>
